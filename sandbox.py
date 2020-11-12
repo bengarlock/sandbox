acc12a05e82af266f0dddf2fa8a60af2d576f091
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-11-12 16:23:07 -0500

    test questions!

--- sandbox.py
+++ sandbox.py
@@ -25,16 +25,17 @@ def find_missing_number(array):
     return missing_numbers
 
 
-#print(find_missing_number([1, 2, 3, 5, 6, 7, 8, 10, 11, 12]))
+# print(find_missing_number([1, 2, 3, 5, 6, 7, 8, 10, 11, 12]))
 
 def find_single_dupe(array):
     return list(set([i for i in array if array.count(i) > 1]))
 
-#print(find_single_dupe([1,2,3,4,4,5,6,7,8,8,9,10]))
+
+# print(find_single_dupe([1,2,3,4,4,5,6,7,8,8,9,10]))
 # [8, 4]
 
 
-#Given two arrays, 1,2,3,4,5 and 2,3,1,0,5 find which number is not present in the second array.
+# Given two arrays, 1,2,3,4,5 and 2,3,1,0,5 find which number is not present in the second array.
 
 def find_common_numbers(array1, array2):
     common = []
@@ -43,19 +44,21 @@ def find_common_numbers(array1, array2):
             common.append(array1[i])
     return list(set(common))
 
-#print(find_common_numbers(array1=[1,2,3,4,5], array2=[2,3,1,0,5]))
-#2,3,4
+
+# print(find_common_numbers(array1=[1,2,3,4,5], array2=[2,3,1,0,5]))
+# 2,3,4
 
 
-#How do you find the second highest number in an integer array?
+# How do you find the second highest number in an integer array?
 def find_second_higest_number(array):
     array.sort()
     return array[-2]
 
-#print(find_second_higest_number([14,200,33,4,12,6,70,44,23,10]))
+
+# print(find_second_higest_number([14,200,33,4,12,6,70,44,23,10]))
 
 
-#How to find all pairs in an array of integers whose sum is equal to the given number?
+# How to find all pairs in an array of integers whose sum is equal to the given number?
 def find_pair_sets(array, number):
     pairs = []
     for i in range(len(array)):
@@ -64,4 +67,49 @@ def find_pair_sets(array, number):
                 pairs.append([array[i], array[j]])
     return pairs
 
-print(find_pair_sets(array=[2,4,6,8,10], number=10))
+
+# print(find_pair_sets(array=[2,4,6,8,10], number=10))
+
+
+def format_date(date):
+    date_split = date.split("/")
+    return date_split[2] + date_split[1] + date_split[0]
+
+
+# print(format_date("11/12/2019"))
+
+def atbash(txt):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                'v', 'w', 'x', 'y', 'z']
+    encrypted_word = []
+
+
+    for i in txt:
+        if i.isupper():
+            lower_case_letter = i.lower()
+            index = alphabet.index(lower_case_letter) * -1 - 1
+            encrypted_word.append(alphabet[index].upper())
+        elif i.islower():
+            index = alphabet.index(i) * -1 - 1
+            encrypted_word.append(alphabet[index])
+        else:
+            encrypted_word.append(i)
+
+    return ''.join(encrypted_word)
+
+
+#print(atbash("Christmas is the 25th of December"))
+
+
+def is_ascending(string):
+
+    for i in range(len(string)):
+        for j in range(i + 1, len(string)):
+            for k in range(j + 1, len(string)):
+                if int(i) + 1 == int(j) and int(j) + 1 == int(k):
+                    return True
+
+    return False
+
+
+print(is_ascending("326325324323"))
