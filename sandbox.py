fe775ebf07386874b567c85bbfa8960714baacd6
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-11-17 09:49:49 -0500

    likes

--- sandbox.py
+++ sandbox.py
@@ -161,12 +161,25 @@ def find_short(s):
 
 
 
-print(find_short("bitcoin take over the world maybe who knows perhaps"))
+#print(find_short("bitcoin take over the world maybe who knows perhaps"))
+
+
+def likes(names):
+    if len(names) == 1:
+        return names[0] + " likes this"
+    elif len(names) == 2:
+        return names[0] + ' and ' + names[1] + " like this"
+    elif len(names) == 3:
+        return names[0] + ', ' + names[1] + ', ' + ' and ' + names[2] + " like this"
+    elif len(names) > 3:
+        return names[0] + ', ' + names[1] + ' and ' + str(len(names)-2) + " others like this"
+    else:
+        return "no one likes this"
 
 
 
 
-
+#print(likes(['Alex', 'Jacob']))
 
 
 
